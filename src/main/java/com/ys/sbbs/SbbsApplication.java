df38ef547751f5a740464aa59acde5214b112d2b
Yumi Kim <yumii2307@naver.com>
2023-07-25 15:17:30 +0900

    first commit

---
+++ src/main/java/com/ys/sbbs/SbbsApplication.java
@@ -0,0 +1,13 @@
+package com.ys.sbbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ys/sbbs/SbbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys.sbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
